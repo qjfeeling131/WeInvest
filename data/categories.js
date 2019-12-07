de5b24abac56cf6197e0e7b4f4f7940986f66aaa
pari Zhong <qjfeeling8994@sina.com>
2019-12-07 21:06:51 +0800

    add the Service Module

---
+++ data/categories.js
@@ -0,0 +1,21 @@
+var data = [{
+  "id": 1,
+  "name": "理财",
+  "icon": '/images/finanical.png'
+}, {
+  "id": 2,
+  "name": '基金',
+  "icon": '/images/finanical.png'
+}, {
+  "id": 3,
+  "name": "债券",
+  "icon": '/images/finanical.png'
+}, {
+  "id": 4,
+  "name": "贷款",
+  "icon": '/images/finanical.png'
+}]
+
+module.exports = {
+  categories: data
+}
---
+++ pages/productList/productList.js
@@ -0,0 +1,95 @@
+const app = getApp()
+const categoriesJson = require('../../data/categories.js');
+const financialProductsJson = require('../../data/financialProducts.js');
+Page({
+
+  data: {
+    nbFrontColor: '#000000',
+    nbBackgroundColor: '#ffffff',
+    activeCategoryId: 0,
+    scrollTop: 0,
+    loadingMoreHidden: true,
+    coupons: [],
+    curPage: 1,
+    pageSize: 10,
+    cateScrollTop: 0,
+    items: [],
+  },
+  onLoad() {
+    this.setData({
+      nbTitle: '新标题',
+      nbLoading: true,
+      nbFrontColor: '#ffffff',
+      nbBackgroundColor: '#000000',
+      categories: categoriesJson.categories,
+    })
+  },
+  tabClick: function(e) {
+    let offset = e.currentTarget.offsetLeft;
+    if (offset > 150) {
+      offset = offset - 150
+    } else {
+      offset = 0;
+    }
+    this.setData({
+      activeCategoryId: e.currentTarget.id,
+      curPage: 1,
+      cateScrollTop: offset
+    });
+    this.getItemsList(this.data.activeCategoryId);
+  },
+
+  getItemsList: function(categoryId, append) {
+    if (categoryId == 0) {
+      categoryId = "";
+    }
+    var that = this;
+    wx.showLoading({
+      "mask": true
+    })
+    switch (categoryId) {
+      case "1":
+        let financialItems = this.paging(this.data.curPage, this.data.pageSize, financialProductsJson.financialProducts)
+        if (financialItems.length > 0) {
+          let items = [];
+          if (append) {
+            items = that.data.items
+          }
+          for (var i = 0; i < financialItems.length; i++) {
+            items.push(financialItems[i]);
+          }
+          that.setData({
+            loadingMoreHidden: true,
+            items: items,
+          });
+        } else {
+          let newData = {
+            loadingMoreHidden: false
+          }
+          if (!append) {
+            newData.items = []
+          }
+          that.setData(newData);
+        }
+        break;
+    }
+    wx.hideLoading()
+  },
+  paging: function(pageNo, pageSize, array) {
+    var offset = (pageNo - 1) * pageSize;
+    return (offset + pageSize >= array.length) ? array.slice(offset, array.length) : array.slice(offset, offset + pageSize);
+  },
+  onReachBottom: function () {
+    this.setData({
+      curPage: this.data.curPage + 1
+    });
+    this.getItemsList(this.data.activeCategoryId, true)
+  },
+  onPullDownRefresh: function () {
+    this.setData({
+      curPage: 1
+    });
+    this.getItemsList(this.data.activeCategoryId)
+    wx.stopPullDownRefresh()
+  }
+})
